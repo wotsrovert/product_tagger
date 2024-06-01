006436051bd709473392b0781bfb427bc65e07a4
Trevor Stow <trevorstow@gmail.com>
2024-06-01 07:20:05 -0700

    add dotenv to load API keys for OpenAi and Pinecone

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts.prompt import PromptTemplate
 from langchain.chains.llm import LLMChain
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from constants import *
+
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+pinecone_api_key = os.getenv("PINECONE_API_KEY")
 
 # the prompt: we will be changing this soon
 prompt = "hello sailor!"
@@ -23,4 +29,6 @@ prompt = "hello sailor!"
 
 # Asking the LLM for a response from our prompt with the provided context using CatOpenAI and invoking it
 # Then print the results content
+print(f"OPENAI: { openai_api_key }")
+print(f"PINECONE: { pinecone_api_key }")
 print( prompt )
